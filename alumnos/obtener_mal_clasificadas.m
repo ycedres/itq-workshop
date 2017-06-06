96ecec641a68d69570d0f543414383d82a97293f
ycedres <ycedres@gmail.com>
2017-06-06 22:36:59 +0100

    WIP live script para alumnos

--- alumnos/obtener_mal_clasificadas.m
+++ alumnos/obtener_mal_clasificadas.m
@@ -1,28 +1,30 @@
-function [mal_clasificadas]=obtener_mal_clasificadas(red_entrenada)
+function [mal_clasificadas_caracteristicas,mal_clasificadas_clasificacion_red,mal_clasificadas_clasificacion_real]=obtener_mal_clasificadas()
 
+red_entrenada=evalin('base','red_entrenada');
 caracteristicas=evalin('base','caracteristicas');
 clasificacion=evalin('base','clasificacion');
 info_entrenamiento=evalin('base','info_entrenamiento');
 
 floresNuevas=caracteristicas(:,info_entrenamiento.testInd);
-[val,idx]=max(red_entrenada(floresNuevas));
+clasificacion_red=red_entrenada(floresNuevas);
+[val,idx]=max(clasificacion_red);
 
 %num_test_samples=size(idx);
+%Entrenar
 prediccion=zeros(3,size(idx,2));
 
 for i=1:size(idx,2)
     prediccion(idx(i),i)=1;
 end
-
+%
 clasificacion_test=clasificacion(:,info_entrenamiento.testInd); 
 
 index_error=clasificacion_test~=prediccion;
 index_error=sum(index_error);
 mal_clasificadas_idx=find(index_error~=0);
-mal_clasificadas=caracteristicas(:,mal_clasificadas_idx);
-% for i=1:size(mal_clasificadas,2)
-%      caracteristicas(:,i)
-% end
-% 
+mal_clasificadas_caracteristicas=caracteristicas(:,mal_clasificadas_idx);
+mal_clasificadas_clasificacion_red=clasificacion_test(:,mal_clasificadas_idx);
+mal_clasificadas_clasificacion_real=clasificacion(:,mal_clasificadas_idx);
+
 
 end
--- alumnos/todo.m
+++ alumnos/todo.m
@@ -1,3 +1,12 @@
+carga_datos_visualizacion;
+figure(1);
+gplotmatrix(caracteristicas, [], clasificacion, ['r', 'b', 'g'],[],[],'off','variable',{'Longitud.Sepalo','Ancho.Sepalo','Longitud.Petalo','Ancho.Petalo'});
+[media,std] = grpstats(caracteristicas,clasificacion,{'mean','std'});
+
 red = define_red;
+view(red);
+
 carga_datos_entrenamiento;
 [red_entrenada,info_entrenamiento] = entrena_red(red,caracteristicas,clasificacion);
+y=red_entrenada(caracteristicas)
+performance = perform(red_entrenada,clasificacion,y)
